09d1197a3ba7e8fafb2a6457051aabc987a9165c
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-06-14 14:12:04 +0500

    done

---
+++ create-user/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container container">
+        <p>All rights reserved. <a href="https://www.youtube.com/@akhrorweb">Samandar To'xtaboyev</a></p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
